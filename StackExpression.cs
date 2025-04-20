7f3af4e7c01eb6fb5119caa51b3b93edbf7706c2
ZygoteCode <zygotecode@proton.me>
2025-04-20 21:48:27 +0200

    First commit

---
+++ StackExpression.cs
@@ -0,0 +1,14 @@
+﻿public class StackExpression
+{
+    public string expression;
+
+    public StackExpression(string expression)
+    {
+        this.expression = expression;
+    }
+
+    public override string ToString()
+    {
+        return this.expression;
+    }
+}
